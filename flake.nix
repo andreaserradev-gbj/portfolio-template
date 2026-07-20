48f5dce206dbd8f43e9afeac4369547c658cf5c5
Andrea Serra <andreaserradev-gbj@users.noreply.github.com>
2026-07-20 06:47:30 +0200

    Add Nix devShell (Node 24) via direnv

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Per-project dev shell";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      forAllSystems = nixpkgs.lib.genAttrs [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [ nodejs_24 ];
+          };
+        }
+      );
+    };
+}
